8307e5b9919304739da84dfd87aa0190d852b8b8
DinooKun <57213778+DinooSaaw@users.noreply.github.com>
2023-06-02 17:30:02 +1000

    add Konami

--- home_page.py
+++ home_page.py
@@ -1,7 +1,28 @@
 import tkinter as tk
+import keyboard
 
 class HomePage(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         label = tk.Label(self, text="Welcome to the Home Page")
         label.pack()
+
+        # Register the Konami code sequence
+        expected_sequence = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"]
+        self.current_index = 0
+
+        def check_sequence(event):
+            if self.focus_get() is None:  # Check if application is not in focus
+                return print("Lost of focus")
+            
+            print(event.name)
+            
+            if event.name == expected_sequence[self.current_index]:
+                self.current_index += 1
+                if self.current_index == len(expected_sequence):
+                    print("Konami code entered!")
+                    self.current_index = 0
+            else:
+                self.current_index = 0
+
+        keyboard.on_press(check_sequence)
