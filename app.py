2982805d47e91353a73b2f5e8a0aa6d2529ea13b
DinooKun <57213778+DinooSaaw@users.noreply.github.com>
2023-06-02 17:07:35 +1000

    aligned the buttons to be in a row and add text to each scren

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ class Application(tk.Tk):
         self.title("Game App")
         self.geometry("400x300")
         
-        # Create buttons
-        self.stats_button = tk.Button(self, text="Stats", command=self.open_stats_page)
-        self.stats_button.pack(pady=10)
+        # Create a frame for buttons
+        button_frame = tk.Frame(self)
+        button_frame.pack(pady=10)
         
-        self.leaderboard_button = tk.Button(self, text="Leaderboard", command=self.open_leaderboard_page)
-        self.leaderboard_button.pack(pady=10)
+        # Create buttons and place them in the frame
+        self.stats_button = tk.Button(button_frame, text="Stats", command=self.open_stats_page)
+        self.stats_button.grid(row=0, column=0, padx=10)
         
-        self.home_button = tk.Button(self, text="Home", command=self.open_home_page)
-        self.home_button.pack(pady=10)
+        self.leaderboard_button = tk.Button(button_frame, text="Leaderboard", command=self.open_leaderboard_page)
+        self.leaderboard_button.grid(row=0, column=1, padx=10)
+        
+        self.home_button = tk.Button(button_frame, text="Home", command=self.open_home_page)
+        self.home_button.grid(row=0, column=2, padx=10)
         
         # Create page containers
         self.stats_page = tk.Frame(self)
@@ -42,6 +46,19 @@ class Application(tk.Tk):
             
         page.pack()
         self.current_page = page
+
+        # Add text to each page
+        if page == self.stats_page:
+            label = tk.Label(page, text="Welcome to the Stats Page")
+            label.pack()
+        
+        elif page == self.leaderboard_page:
+            label = tk.Label(page, text="Welcome to the Leaderboard Page")
+            label.pack()
+        
+        elif page == self.home_page:
+            label = tk.Label(page, text="Welcome to the Home Page")
+            label.pack()
 
 # Run the application
 app = Application()
