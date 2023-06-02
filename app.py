8e9ff7a6427b2a3a3bb59eb208c3c5c73a677aa7
DinooKun <57213778+DinooSaaw@users.noreply.github.com>
2023-06-02 20:27:16 +1000

    added login page

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from stats_page import StatsPage
 from leaderboard_page import LeaderboardPage
 from home_page import HomePage
+from login_page import LoginPage
+
 
 class Application(tk.Tk):
     def __init__(self):
@@ -15,21 +17,35 @@ class Application(tk.Tk):
         button_frame.pack(pady=10)
 
         # Create buttons and place them in the frame
-        self.stats_button = tk.Button(button_frame, text="Stats", command=self.open_stats_page)
+        self.stats_button = tk.Button(
+            button_frame, text="Stats", command=self.open_stats_page)
         self.stats_button.grid(row=0, column=0, padx=10)
 
-        self.leaderboard_button = tk.Button(button_frame, text="Leaderboard", command=self.open_leaderboard_page)
+        self.leaderboard_button = tk.Button(
+            button_frame, text="Leaderboard", command=self.open_leaderboard_page)
         self.leaderboard_button.grid(row=0, column=1, padx=10)
 
-        self.home_button = tk.Button(button_frame, text="Home", command=self.open_home_page)
+        self.home_button = tk.Button(
+            button_frame, text="Home", command=self.open_home_page)
         self.home_button.grid(row=0, column=2, padx=10)
 
         # Create page containers
         self.stats_page = None
         self.leaderboard_page = None
         self.home_page = None
+        self.login_page = None
 
         self.current_page = None
+        self.is_konami_code_active = False
+        self.is_logged_in = False  # Variable to track login status
+
+        # Create a label to display the username
+        self.username_label = tk.Label(self, text="Username: Not Logged In", font=("Arial", 10))
+        self.username_label.pack(side=tk.BOTTOM, anchor=tk.W, padx=10, pady=10)
+
+        # Create the login button
+        self.login_button = tk.Button(
+            self, text="Login", command=self.open_login_page)
 
         # Show home page by default
         self.open_home_page()
@@ -60,15 +76,44 @@ class Application(tk.Tk):
         if self.current_page:
             self.current_page.pack_forget()
 
+        if self.is_logged_in:
+            self.username_label.pack(side=tk.BOTTOM, anchor=tk.W, padx=10, pady=10)
+        else:
+            self.username_label.pack_forget()
+
+        self.login_button.pack_forget()  # Hide the login button
+
         if self.home_page is None:
-            self.home_page = HomePage(self)
+            self.home_page = HomePage(self, self.konami_code_callback)
 
         self.home_page.pack()
         self.current_page = self.home_page
         self.update_title("Home")
 
+    def open_login_page(self):
+        if self.current_page:
+            self.current_page.pack_forget()
+
+        if self.login_page is None:
+            self.login_page = LoginPage(self, self.login_callback)
+
+        self.login_page.pack()
+        self.current_page = self.login_page
+        self.update_title("Login")
+
     def update_title(self, page_title):
-        self.title(f"ALC ~ {page_title}")
+        self.title(f"Game App ~ {page_title}")
+
+    def konami_code_callback(self):
+        self.is_konami_code_active = True
+        self.login_button.pack(side=tk.TOP, anchor=tk.E, padx=10, pady=10)
+
+    def login_callback(self, username):
+        self.is_logged_in = True
+        self.username_label.config(text=f"Username: {username}")
+        self.open_home_page()
+        self.username_label.pack(side=tk.BOTTOM, anchor=tk.W, padx=10, pady=10)
+        self.login_button.pack_forget()  # Hide the login button after successful login
 
 
 # Run the application
--- home_page.py
+++ home_page.py
@@ -2,10 +2,12 @@ import tkinter as tk
 import keyboard
 
 class HomePage(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, callback):
         super().__init__(parent)
         label = tk.Label(self, text="Welcome to the Home Page")
         label.pack()
+
+        self.callback = callback
 
         # Register the Konami code sequence
         expected_sequence = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"]
@@ -16,12 +18,12 @@ class HomePage(tk.Frame):
                 return print("Lost of focus")
             
             print(event.name)
-            
-            if event.name == expected_sequence[self.current_index]:
+            if event.name == expected_sequence[self.current_index].lower():
                 self.current_index += 1
                 if self.current_index == len(expected_sequence):
                     print("Konami code entered!")
                     self.current_index = 0
+                    self.callback()  # Call the callback function in Application
                     keyboard.unhook_all()
             else:
                 self.current_index = 0
